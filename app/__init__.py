21c5bf8e4c1399b045d8daaa64ed3c7f0251421c
soldatsixtyseven <145689190+soldatsixtyseven@users.noreply.github.com>
2023-11-02 14:43:26 +0100

    page lien home 02.11.23

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,7 @@ def create_app():
     # Chargement des variables de configuration stockées dans le fichier config.py
     app.config.from_pyfile(os.path.join(os.path.dirname(__file__), "config.py"))
 
-    # Enreigstrement des blueprints de l'application.
+    # Enregistrement des blueprints de l'application.
     app.register_blueprint(home_bp)
     app.register_blueprint(auth_bp)
     app.register_blueprint(user_bp)
--- app/views/home.py
+++ app/views/home.py
@@ -15,3 +15,14 @@ def landing_page():
 @home_bp.route('/<path:text>', methods=['GET', 'POST'])
 def not_found_error(text):
     return render_template('home/404.html'), 404
+
+# Route /sports
+@home_bp.route('/sports', methods=['GET', 'POST'])
+def sports_page():
+    return render_template('home/sports.html')
+
+# Route /contact
+@home_bp.route('/contact', methods=['GET', 'POST'])
+def contact_page():
+    return render_template('home/contact.html')
+
