743227cab20ada620e7eaba994fd02a08fc36381
soldatsixtyseven <frossardtheo@gmail.com>
2024-03-24 23:40:53 +0100

    modification de la clée secrète

--- app/config_example.py
+++ app/config_example.py
@@ -1,5 +1,5 @@
 # Clée secrète
-SECRET_KEY= 'lkKvtgwyUswdmAcgbfc4du4A65BW4uZN' #example de clée secrète
+SECRET_KEY= '4a4dff2f3450e2e032b9fa8b9f46214f' #example de clée secrète
 
 # Connection à la base de donné
 DATABASE= 'ma_base_de_données.db'
